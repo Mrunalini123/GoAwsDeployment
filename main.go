20929d006e22e77769715e1e26a268689b786c62
RECORDSHUB_SPEDRMS@ssloman.com <RECORDSHUB_SPEDRMS@ssloman.com>
2025-07-07 19:58:59 +0530

    Fifth commit

--- main.go
+++ main.go
@@ -9,5 +9,5 @@ func main() {
 	http.ListenAndServe(":8976", router)
 }
 func greetUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World !!!"))
+	w.Write([]byte("Hello World !!!!"))
 }
